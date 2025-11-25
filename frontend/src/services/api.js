c818922d68698a96ee3965b1344fbf8612ecd949
Sandeep Yadav <103882286+sandeepyadav-24@users.noreply.github.com>
2025-11-26 03:14:36 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://task-management-stbr.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
